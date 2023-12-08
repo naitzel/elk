6ceea923996d3b2274dd8ff59e1318af8c30c5dd
Rogério Adriano <rogerioadris@gmail.com>
2023-12-08 00:20:16 -0300

    Adicionado kibana com SSL

--- docker/setup/initial.sh
+++ docker/setup/initial.sh
@@ -1,3 +1,10 @@
+if [ x${ELASTIC_PASSWORD} == x ]; then
+    echo "Set the ELASTIC_PASSWORD environment variable in the .env file";
+    exit 1;
+elif [ x${KIBANA_PASSWORD} == x ]; then
+    echo "Set the KIBANA_PASSWORD environment variable in the .env file";
+    exit 1;
+fi;
 
 if [ ! -f config/certs/ca.zip ]; then
     echo "Creating CA";
@@ -22,3 +29,12 @@ fi;
 chown -R 1000:1000 config/certs;
 find . -type d -exec chmod 750 \{\} \;;
 find . -type f -exec chmod 640 \{\} \;;
+
+echo "Waiting for Elasticsearch availability";
+until curl -s --cacert config/certs/ca/ca.crt https://elasticsearch:9200 | grep -q "missing authentication credentials"; do sleep 30; done;
+
+echo "Setting kibana_system password";
+until curl -s -X POST --cacert config/certs/ca/ca.crt \
+    -u "elastic:${ELASTIC_PASSWORD}" \
+    -H "Content-Type: application/json" https://elasticsearch:9200/_security/user/kibana_system/_password \
+    -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}"; do sleep 10; done;
